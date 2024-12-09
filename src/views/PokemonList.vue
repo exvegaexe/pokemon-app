1cf1e10cfbe378e2b1ea0076b71fc6e7d1072f3b
Afiq <hazrulafiq123@gmail.com>
2024-12-09 11:10:45 +0800

    add comment

--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container my-4">
 
-    <!--logo-->
+
+
+    <!--logotest-->
     <div class="text-center mb-4">
       <img src="../assets/img/logo.png" alt="Pokémon Logo" class="img-fluid" />
     </div>
